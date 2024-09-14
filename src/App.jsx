60f73eb9c778263a3bbf2335c46df3be6e5e07de
amarkumarsbg <amarkumarsbg13@gmail.com>
2024-09-14 21:31:49 +0530

    Project completed

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <main className="overflow-x-hidden ">
+      <Hero />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/data/MockData.js
@@ -0,0 +1,59 @@
+import Img1 from "../assets/1.png";
+import Img2 from "../assets/2.png";
+import Img3 from "../assets/3.png";
+
+export const NavbarData = [
+  {
+    id: 1,
+    title: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    title: "Categories",
+    link: "#",
+  },
+  {
+    id: 3,
+    title: "Blogs",
+    link: "#",
+  },
+  {
+    id: 2,
+    title: "Contact",
+    link: "#",
+  },
+];
+
+export const HeadphoneData = [
+  {
+    id: 1,
+    image: Img1,
+    title: "Apple Airpods Wireless Over-Ear Headphones",
+    subtitle:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+    price: "$40.99",
+    modal: "Airpods",
+    bgColor: "#e85950",
+  },
+  {
+    id: 2,
+    image: Img2,
+    title: "Apple Airpods Wireless Over-Ear Headphones",
+    subtitle:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+    price: "$100",
+    modal: "Max",
+    bgColor: "#24b4bc",
+  },
+  {
+    id: 3,
+    image: Img3,
+    title: "Apple Airpods Wireless Over-Ear Headphones",
+    subtitle:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+    price: "$140.99",
+    modal: "Pro",
+    bgColor: "#7aa10e",
+  },
+];
